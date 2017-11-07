b4a21bb713f167aff054b453c56857ba331e16af
Joshua Smith Laptop <joshner.smith@gmail.com>
2017-11-07 00:00:08 -0800

    - Testing new version

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 pthread_t threads[NUM_THREADS];
 
 // First, we list our two sempahores
-sem_t increaseHeight; // Create semaphore increaseHeight
-sem_t decreaseHeight; // Create semaphore decreaseHeight
+sem_t sem1; // Create semaphore sem1
+sem_t sem2; // Create semaphore sem2
 
 // We'll store the heights for each person here
 float fredHeight = 1; // Fred (Person A) starts on the low end, one foot off the ground
@@ -18,58 +18,40 @@ float wilmaHeight = 7; // Wilma (Persona B) starts on the high end, seven feet o
 
 void *fredSee(void*) { // Fred's Behavior
 
-    // Go up and down 10 times
-    for (int i = 0; i < 10; i++) {
+    while(true) {
         // Go up
-        sem_wait(&increaseHeight); // Grab the semaphore for increaseHeight when allowed, and lock it
-        while(fredHeight < 7) { // Maximum height is 7 feet
-            fredHeight += 1;
-            cout << "Fred Height: " << fredHeight << endl;
-            sleep(1);
-        }
+        sem_wait(&sem1); // Grab the semaphore for sem1 when allowed, and lock it
+            if(fredHeight < 7)
+                fredHeight += 1;
+            else if(fredHeight > 1)
+                fredHeight -= 1.5;
+        sem_post(&sem2);
 
-        sem_post(&increaseHeight); // Release the semaphore for increase height
-        // Go down
-        sem_wait(&decreaseHeight);
-        while(fredHeight > 1) { // Check to see if fred is within a foot off the ground. If not, decrease height
-            fredHeight -= 1.5;
-            cout << "Fred Height: " << fredHeight << endl;
-            sleep(1);
-        }
-
-        sem_post(&decreaseHeight); // Release the semaphore for decrease height
+        sleep(1);
+        cout << "first thread" << endl;
+        pthread_exit(nullptr);
     }
 
-    cout << "first thread" << endl;
-    pthread_exit(nullptr);
 
 }
 void *wilmaSaw(void*) { // Wilma's Behavior
 
-    // Go up and down 10 times
-    for (int i = 0; i < 10; i++) {
-        // Go down (when you are allowed to
-        sem_wait(&decreaseHeight);
-        while(wilmaHeight > 1) {
-            wilmaHeight -= 1;
-            cout << "Wilma Height: " << wilmaHeight << endl;
-            sleep(1);
-        }
+    while(true) {
+        // Go up
+        sem_wait(&sem2); // Grab the semaphore for sem1 when allowed, and lock it
 
-        sem_post(&decreaseHeight); // Release the semaphore for decrease height
-        // Go up (when you are allowed to go up because you are
-        sem_wait(&increaseHeight); // Grab the semaphore for increaseHeight when allowed, and lock it
-        while(wilmaHeight < 7) { // Maximum height is 7 feet
-            wilmaHeight += 1.5;
-            cout << "Wilma Height: " << wilmaHeight << endl;
-            sleep(1);
-        }
+        if(fredHeight > 1)
+            fredHeight -= 1;
+        else if(wilmaHeight < 7)
+            fredHeight += 1.5;
+        sem_post(&sem1);
 
-        sem_post(&increaseHeight); // Release the semaphore for increase height
+        sleep(1);
+        cout << "first thread" << endl;
+        pthread_exit(nullptr);
     }
 
-    cout << "second thread" << endl;
-    pthread_exit(nullptr);
+
 }
 
 void checkForThreadErrors(int tid) {
@@ -81,8 +63,8 @@ void checkForThreadErrors(int tid) {
 
 int main(int argc, char** argv) {
 
-    sem_init(&increaseHeight, 1, 1); // Initialize increaseHeight semaphore, shared between threads, and initialize value to 1
-    sem_init(&decreaseHeight, 1, 1); // Initialize decreaseHeight semaphore, shared between threads, and initialize value to 1
+    sem_init(&sem1, 1, 1); // Initialize sem1 semaphore, shared between threads, and initialize value to 1
+    sem_init(&sem2, 1, 0); // Initialize sem2 semaphore, shared between threads, and initialize value to 1
 
     int tid;
 
